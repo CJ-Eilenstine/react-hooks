4eb8fe8dfb033d82925a6d39991ab2062f90683c
CJ-Eilenstine <115022708+CJ-Eilenstine@users.noreply.github.com>
2024-09-18 11:31:19 -0400

    Poke Search
    Started working on a function to be able to search for Pokemon.

--- src/app/pokemon/page.js
+++ src/app/pokemon/page.js
@@ -1,7 +1,47 @@
+import pokeStyles from "./pokemon.module.css";
+import useState from "react";
+
+// Pokemon Date
+/**
+ * @typedef {Object} pokemonApiObject
+ * @prop {String} name Name of Pokemon
+ * @prop {Sprite} sprites Object with all sprite
+ * @prop {String} sprites.front_default
+ * @prop {Number} height Multiply by 10
+ * @prop {Number} weight Divide by 10 to make kg
+ */
+
 export default function Pokemon() {
+  /**
+   * @type {[pokemonApiObject, Function]}
+   */
+  const [pokemon, setPokemon] = useState({});
+  /**
+   * @type {[String, Function]}
+   */
+  const [searchTerm, setSearchTerm] = useState("");
+
+  function changeSearchTerm(e) {
+    setSearchTerm(e.currentTarget.value);
+  }
+
+  async function searchForPokemonByName() {
+    try {
+    } catch (error) {}
+
+    const rawData = await fetch(
+      `https://pokeapi.co/api/v2/pokemon//${searchTerm}`
+    );
+    const pokeDataFormatted = await rawData.json();
+  }
+
   return (
     <main>
       <h1>Pokemon Page</h1>
+      <div className={pokeStyles.search}>
+        <input type="search" id="search" name="search" />
+        <input type="button" value="search" />
+      </div>
     </main>
   );
 }
